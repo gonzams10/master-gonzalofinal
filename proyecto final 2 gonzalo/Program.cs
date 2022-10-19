cdd5b023dc415f8c5257ecc0b06447e0a42bd249
Gonzalo Sebastian Mendez <gonzams10@gmail.com>
2022-10-18 23:00:40 -0300

    ultima

--- proyecto final 2 gonzalo/Program.cs
+++ proyecto final 2 gonzalo/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using proyecto_final_2_gonzalo.ADO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,7 +11,24 @@ namespace proyecto_final_2_gonzalo
     {
         static void Main(string[] args)
         {
+            ADOUsuario user = new ADO.ADOUsuario();
+            user.TraerUsuario("tcasazza");
+            Console.ReadKey();
+            Console.Clear();
 
+            ADOProducto producto = new ADOProducto();
+            producto.TraerProductoByUsuario(1);
+            Console.ReadKey();
+            Console.Clear();
+
+            ADOProductoVendido productoVendido = new ADOProductoVendido();
+            productoVendido.ProdutoVendidoByUsuario("tcasazza");
+            Console.ReadKey();
+            Console.Clear();
+
+            ADOVenta venta = new ADOVenta();
+            venta.TraerVentaPorUsuario(1);
+            Console.ReadKey();
 
 
         }
